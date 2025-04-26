bd16b3d037f92531936f0592630555c4c07c00ae
mouaadabid21 <mouaadabid21@gmail.com>
2025-04-26 22:55:16 +0100

    change user api

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'https://reqres.in/api/users';
+  private apiUrl = 'https://dummyjson.com/users';
 
   constructor(private http: HttpClient) {}
 
